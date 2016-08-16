67c26aa9b14a906081ebe26c4878dd143d6cbc98
bingjie.huang <bingjie.huang@shuyun.com>
2016-08-16 15:33:50 +0800

    add typings folder

---
+++ typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="react/react.d.ts" />
+/// <reference path="es6-shim/es6-shim.d.ts" />
+/// <reference path="react/react-dom.d.ts" />
+/// <reference path="shallowequal/shallowequal.d.ts" />
