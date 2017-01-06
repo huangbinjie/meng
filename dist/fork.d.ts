28fa97da1df70837b153678ae73d29b41b00cf7b
bingjie.huang <bingjie.huang@shuyun.com>
2017-01-06 16:05:13 +0800

    3.1.8 release

--- dist/fork.d.ts
+++
@@ -1,8 +0,0 @@
-import { Observable, ReplaySubject } from 'rxjs';
-import { Resource, Success } from './inject';
-export declare function fork<S>(state$: ReplaySubject<S>, {source$, success}: Resource): Observable<S>;
-export declare const combineLatestSelector: (acc: Object, x: Object) => Object;
-export declare const resetCallback: (state: Object) => Object & {
-    callback: () => void;
-};
-export declare const implSelector: (success: Success) => (state: Object) => Object;
