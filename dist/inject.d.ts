5707ff35ad265aab79ff73038f234c296e691f74
bingjie.huang <bingjie.huang@shuyun.com>
2017-04-24 17:32:13 +0800

    3.3.1

--- dist/inject.d.ts
+++ dist/inject.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="react" />
 import { ObservableInput } from "rxjs/Observable";
 import { ComponentClass } from "react";
-export declare type Inject = ObservableInput<Object> | (() => any) | Object;
+export declare type Inject = ObservableInput<Object> | (() => any) | any;
 export declare type Listen<S> = (currentStore: Partial<S>, nextStore: Partial<S>) => Inject;
 export declare type Success<S> = string | ((currentState: S, state: Partial<S>) => object);
 export declare type AsyncResource<S> = {
@@ -12,5 +12,5 @@ export declare type ListenResource<S> = {
     source$: Listen<S>;
     success: Success<S>;
 };
-export declare const inject: <S>(source$: Inject, success: Success<S>) => <S>(component: ComponentClass<S>) => any;
+export declare const inject: <S>(source$: any, success: Success<S>) => <S>(component: ComponentClass<S>) => any;
 export declare const listen: <S>(source$: Listen<S>, success: Success<S>) => <S>(component: ComponentClass<S>) => any;
