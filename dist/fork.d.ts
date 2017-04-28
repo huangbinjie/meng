3c0b189df49d142458f37a07086d44b5f3ece2e4
bingjie.huang <bingjie.huang@shuyun.com>
2017-04-28 19:15:30 +0800

    3.3.2

--- dist/fork.d.ts
+++ dist/fork.d.ts
@@ -4,4 +4,6 @@ import { Observable } from "rxjs";
 import { AsyncResource, ListenResource, Success } from "./inject";
 export declare function forkAsync<S extends object, C extends Component<Partial<S>, S>>(this: C, {source$, success}: AsyncResource<S>): Observable<S>;
 export declare function forkListen<S extends object, C extends Component<Partial<S>, S>>(this: C, {source$, success}: ListenResource<S>, store$: Observable<[Partial<S>, Partial<S>]>): Observable<S>;
-export declare function implSelector<S, C extends Component<Partial<S>, S>>(this: C, state: Partial<S>, success: Success<S>): object;
+export declare function implSelector<S, C extends Component<Partial<S>, S>, K extends keyof S>(this: C, state: S[K], success: Success<S>): Partial<S> | {
+    [x: string]: S[K];
+};
--- dist/inject.d.ts
+++ dist/inject.d.ts
@@ -3,7 +3,7 @@ import { ObservableInput } from "rxjs/Observable";
 import { ComponentClass } from "react";
 export declare type Inject = ObservableInput<Object> | (() => any) | any;
 export declare type Listen<S> = (currentStore: Partial<S>, nextStore: Partial<S>) => Inject;
-export declare type Success<S> = string | ((currentState: S, state: Partial<S>) => object);
+export declare type Success<S> = string | ((currentState: S, value: S[keyof S]) => Partial<S>);
 export declare type AsyncResource<S> = {
     source$: Inject;
     success: Success<S>;
